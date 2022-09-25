c625041a29dfbb8b6fd4ed999e6b3e2fe46517a9
SupineCup <114007597+SupineCup@users.noreply.github.com>
2022-09-25 22:01:49 +0300

    Add files via upload

---
+++ Project3/Source.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int main(void) {
+    int A, B, C;
+    printf("A:");
+    scanf("%i", &A);
+    printf("B:");
+    scanf("%i", &B);
+    printf("C:");
+    scanf("%i", &C);
+    printf("%d\n", (A > 0) + (B > 0) + (C > 0) == 2);
+    return 0;
+
+#include <iostream>
+    using namespace std;
+    int main()
+        //setlocale(LC_ALL, "Rus");
+        //int A;
+        //cout << "A:";
+        //cin >> A;
+        //if (A % 2 == 0 && A > 9 && A < 100) 
+        //cout << "Истинно";
+        //else
+        //cout << "Ложь";
+
+#include <iostream>
+//using namespace std;
+
+//int main() 
+    //setlocale(LC_ALL, "Rus");
+    //int A;
+    //cout << "A:";
+    //cin >> A;
+    //if (A % 2 != 0 && A > 99 && A < 1000) 
+        //cout << "Истинно";
+    //else
+        //cout << "Ложь";
+
+#include <stdio.h>
+
+    //int main(void)
+    //int A;
+    //printf("A:");
+    //scanf("%i", &A);
+    //A = abs(A);
+    //printf("%d\n", ((A / 100) != (A % 100 / 10)) & ((A / 100) != (A % 10)) & ((A % 100 / 10) != (A % 10)));
+    //return 0;\
+        
+#include <iostream>
+#include <math.h>
+        //using namespace std;
+    //int main()
+    //{
+        //int A;
+        //cout << "A: ";
+        //cin >> A;
+        //A = fabs(A);
+        //cout << (((A / 100) < (A % 100 / 10)) & ((A % 100 / 10) < (A % 10))) << endl;
+        //return 0;
